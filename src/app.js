c0476039492a70099cdbb581a744b4ac23cf0bc1
subhajit saha <saha3475@gmail.com>
2026-01-26 12:22:52 +0530

    cors update

--- src/app.js
+++ src/app.js
@@ -8,12 +8,14 @@ import fileUpload from "express-fileupload";
 import { fileURLToPath } from "url";
 import "./config/passport.js";
 import path from "path";
+import cors from "cors";
+
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 const app = express();
-
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(
