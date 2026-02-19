70045281b9d032afd22041bbc7e5c5d2f785672c
subhajit saha <saha3475@gmail.com>
2026-02-19 21:38:57 +0530

    cors issue fix

--- src/app.js
+++ src/app.js
@@ -15,7 +15,13 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 const app = express();
-app.use(cors());
+app.use(cors({
+  origin: ['https://www.tourguidetrack.in', 'https://tourguidetrack.in'],
+  methods: ['GET','POST','PUT','DELETE','OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+  credentials: true
+}));
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(
